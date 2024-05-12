0e3be7d051456467a9212639e4e49d2668ba0fb1
Roman Kolychev <demozbox@gmail.com>
2024-05-13 03:22:43 +0700

    Added reactive state

---
+++ Events/DMZState.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+
+namespace DMZ.Events
+{
+    public class DMZState<T> : IDisposable
+    {
+        private readonly List<Action<T>> _subscribers = new();
+        protected readonly object _lock = new();
+        protected T _data;
+
+        public void Subscribe(Action<T> callback)
+        {
+            lock (_lock)
+            {
+                _subscribers.Add(callback);
+            }
+        }
+
+        public void Unsubscribe(Action<T> callback)
+        {
+            lock (_lock)
+            {
+                _subscribers.Remove(callback);
+            }
+        }
+
+        public T Value
+        {
+            get => _data;
+            set
+            {
+                lock (_lock)
+                {
+                    if (Equals(value, _data))
+                        return;
+
+                    _data = value;
+                }
+
+                NotifySubscribers();
+            }
+        }
+
+        protected virtual void NotifySubscribers()
+        {
+            List<Action<T>> subscribersCopy;
+
+            lock (_lock)
+            {
+                subscribersCopy = new List<Action<T>>(_subscribers);
+            }
+
+            foreach (var subscriber in subscribersCopy)
+            {
+                subscriber?.Invoke(_data);
+            }
+        }
+
+        public void Dispose()
+        {
+            lock (_lock)
+            {
+                _subscribers.Clear();
+            }
+        }
+    }
+}
--- Extensions/TaskExtensions.cs
+++ Extensions/TaskExtensions.cs
@@ -3,12 +3,24 @@ using System.Threading.Tasks;
 
 namespace DMZ.Extensions
 {
-	public static class TaskExtensions
-	{
-		public static bool IsSucceeded(this Task task)
-			=> task.IsCompleted && !task.IsFaulted && !task.IsCanceled;
+    public static class TaskExtensions
+    {
+        public static bool IsSucceeded(this Task task)
+            => task.IsCompleted && !task.IsFaulted && !task.IsCanceled;
 
-		public static Task ContinueOnMainThread<T>(this Task<T> task, Action<Task<T>> action)
-			=> task.ContinueWith(action, TaskScheduler.FromCurrentSynchronizationContext());
-	}
+        public static Task ContinueOnMainThread<T>(this Task<T> task, Action<Task<T>> action)
+            => task.ContinueWith(action, TaskScheduler.FromCurrentSynchronizationContext());
+
+
+        public static void Forget(this Task task, Action<Exception> errorHandler = null)
+        {
+            task.ContinueWith(t =>
+            {
+                if (t.IsFaulted && errorHandler != null)
+                {
+                    errorHandler(t.Exception);
+                }
+            }, TaskContinuationOptions.OnlyOnFaulted);
+        }
+    }
 }
